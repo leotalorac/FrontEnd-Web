ca0848dd34f16624a19ead93927b4013ef2bd07b
marcestone <dmcifuentesp@unal.edu.co>
2020-11-10 20:36:05 -0500

    forum with https

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // IP's
-const FORUM_IP = "http://52.200.134.90:3000"
+const FORUM_IP = "https://52.200.134.90:3000"
 const GraphQL_URL = "http://3.138.86.155/graphql"
 //URLS
 const URL_FORUMS = `${FORUM_IP}/forums`;
